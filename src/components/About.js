d1d82385fda97eb615c72e8c542ab6aff1e17c16
NicoleWrz <nicolew96@gmail.com>
2022-02-04 16:34:45 -0500

    finished about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About() {
   return (
       <Container className="about-container">
         <Row>
-          <Col lg={true} className="about-img text-center">
+          <Col lg={true} className="about-img text-center"> 
             <img
               src={Avatar}
               className="emote"
@@ -18,15 +18,15 @@ export default function About() {
           <Col lg={true} className="bio">
             <div>
               <h1>Hi, I'm Nicole.</h1>
-              <h1>I love building creative and engaging websites.</h1>
-              <p>
+              <h3>I love building creative and engaging websites.</h3>
+              <p className="description">
                 I'm a passionate Full Stack Web Developer with experience
                 building web applications with JavaScript, React, Express, HTML,
                 CSS and other technologies and frameworks.
               </p>
-              <div className="flex justify-center">
+              <div className="text-center">
                 <a
-                  href="./Nicole-Wrzosek-Resume.pdf"
+                  href="https://drive.google.com/file/d/1DTYCca3AqmdCfLkBhbHIVv_aGjKtY9Z0/view?usp=sharing"
                   target="_blank"
                   className="resumeButton"
                 >
