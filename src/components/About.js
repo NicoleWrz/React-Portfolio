e21852dc3482b1fe8fb236ab1d298715866c0dc1
NicoleWrz <nicolew96@gmail.com>
2022-02-04 16:08:39 -0500

    updated about with styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,41 @@
 import React from "react";
-import emote from '../images/emote.png';
-import Card from 'react-bootstrap/Card';
+import { Container, Row, Col } from "react-bootstrap";
+import Avatar from "../images/emote.png";
 
 export default function About() {
-    return (
-        <div className='about'>
-        <div className='bio'>
-            <div className='emote'>
-                <img src={emote} alt='grey alien avatar' />
+  return (
+      <Container className="about-container">
+        <Row>
+          <Col lg={true} className="about-img text-center">
+            <img
+              src={Avatar}
+              className="emote"
+              alt="Emote"
+              width="450"
+              height="450"
+            ></img>
+          </Col>
+          <Col lg={true} className="bio">
+            <div>
+              <h1>Hi, I'm Nicole.</h1>
+              <h1>I love building creative and engaging websites.</h1>
+              <p>
+                I'm a passionate Full Stack Web Developer with experience
+                building web applications with JavaScript, React, Express, HTML,
+                CSS and other technologies and frameworks.
+              </p>
+              <div className="flex justify-center">
+                <a
+                  href="./Nicole-Wrzosek-Resume.pdf"
+                  target="_blank"
+                  className="resumeButton"
+                >
+                  Resume
+                </a>
+              </div>
             </div>
-            <Card className='aboutBody border-0'>
-                <Card.Body>
-                    <div className='aboutBio'>
-                        <h1> Hi, I'm Nicole.</h1>
-                        I'm a passionate Full Stack Web Developer with experience building web applications 
-                        with JavaScript, React, Express, HTML, CSS and other technologies and frameworks. 
-                        <a href="./Nicole-Wrzosek-Resume.pdf" className="resumeButton">Resume</a>
-                    </div>
-                </Card.Body>
-            </Card>
-        </div>
-    </div>
-    )
-    
+          </Col>
+        </Row>
+      </Container>
+  );
 }
