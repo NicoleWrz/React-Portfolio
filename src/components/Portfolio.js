159af2003b40f6e11b0297caf8645ec28280aab2
NicoleWrz <nicolew96@gmail.com>
2022-04-14 20:28:33 -0400

    added projects to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import { FaCode } from 'react-icons/fa';
+import Food from "../images/FoodForDays.gif";
+import Pokemon from "../images/pokefinder.gif"
+import { Container } from "react-bootstrap";
+import Project from "../Project";
 
 export default function Portfolio() {
-  return(
-    <div className='portfolio'>
-      <h2 className="portfolio-title"> <FaCode className="workicon"/> Portfolio <FaCode className="workicon"/> </h2>
 
-    </div>
+    const projects = [
+      {
+        title: "Food For Days",
+        technologies: "JavaScript, Node, Express",
+        description: "Users are presented with a list of cuisines with meals to choose from. They can save any recipe to their own profile after signing up/logging in.",
+        gif: Food,
+        github: "https://github.com/quynhndinh/Food-For-Days",
+        link: "https://food-for-days.herokuapp.com/"
+      },
+      {
+        title: "PokéFinder",
+        technologies: "HTML, CSS, Javascript",
+        description: "Users are presented with a card and a list of encounter locations of their desired Pokémon.",
+        gif: Pokemon,
+        github: "https://github.com/bchen41/PokeFinder",
+        link: "https://bchen41.github.io/PokeFinder/"
+      }
+    ]
+
+  
   )
 }
